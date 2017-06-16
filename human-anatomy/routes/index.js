f83b1985f11f3210682348fdf167688ddbb8dc4e
ericVargas <batman@Erics-MacBook-Pro-2.local>
2017-06-16 11:08:47 -0400

    made a unique route for each table to be called

--- human-anatomy/routes/index.js
+++ human-anatomy/routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 var db = require('../queries');
 
 
-router.get('/cardio', db.getAllCardio);
+router.get('/', db.getCardio);
+
+router.get('/digestive', db.getDigest);
+
+router.get('/skeletal', db.getSkeletal);
+
+router.get('/musculoskeletal', db.getMuscle);
+
+router.get('/respiratory', db.getResp);
+
+router.get('/nervous', db.getNervous);
+
+router.get('/integumentary', db.getInteg);
+
+router.get('/lymphatic', db.getLymph);
+
+router.get('/endocrine', db.getEndo);
+
+router.get('/urinary', db.getUrinary);
+
+router.get('/reproductive', db.getReproduce);
+
+router.get('/direction', db.getDirect);
+
+router.get('/eye', db.getEye);
 
 
 module.exports = router;
